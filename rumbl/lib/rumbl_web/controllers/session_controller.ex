41b1176b63b1b3688c9c59c4a9d1973ca561254b
Samyr Ribeiro <saamyr@live.com>
2024-04-21 18:13:02 -0300

    feat: added sessions and authentication features (register,login,logout)

---
+++ rumbl/lib/rumbl_web/controllers/session_controller.ex
@@ -0,0 +1,28 @@
+defmodule RumblWeb.SessionController do
+  use RumblWeb, :controller
+
+  def new(conn, _) do
+    render(conn, "new.html")
+  end
+
+  def create(conn, %{"session" => %{"username" => username, "password" => pw}}) do
+    case Rumbl.Accounts.authenticate_by_username_and_pass(username, pw) do
+      {:ok, user} ->
+        conn
+        |> RumblWeb.Auth.login(user)
+        |> put_flash(:info, "Welcome back!")
+        |> redirect(to: "/")
+
+      {:error, _reason} ->
+        conn
+        |> put_flash(:error, "Invalid username/password combination")
+        |> render("new.html")
+    end
+  end
+
+  def delete(conn, _) do
+    conn
+    |> RumblWeb.Auth.logout()
+    |> redirect(to: "/")
+  end
+end
