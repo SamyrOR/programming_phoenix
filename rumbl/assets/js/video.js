15ac6251dd7696a45e0745e72fa6a1059fe14921
Samyr Ribeiro <saamyr@live.com>
2024-06-13 23:40:34 -0300

    feat: added time and seek to in the annotations of a video

--- rumbl/assets/js/video.js
+++ rumbl/assets/js/video.js
@@ -17,7 +17,10 @@ let Video = {
     let msgContainer = document.getElementById("msg-container");
     let msgInput = document.getElementById("msg-input");
     let postButton = document.getElementById("msg-submit");
-    let vidChannel = socket.channel("videos:" + videoId);
+    let lastSeenId = 0;
+    let vidChannel = socket.channel("videos:" + videoId, () => {
+      return { last_seend_id: lastSeenId };
+    });
     postButton.addEventListener("click", (e) => {
       let payload = { body: msgInput.value, at: Player.getCurrentTime() };
       vidChannel
@@ -25,6 +28,17 @@ let Video = {
         .receive("error", (e) => console.log(e));
       msgInput.value = "";
     });
+    msgContainer.addEventListener("click", (e) => {
+      e.preventDefault();
+      let seconds =
+        e.target.getAttribute("data-seek") ||
+        e.target.parentNoed.getAttribute("data-seek");
+      if (!seconds) {
+        return;
+      }
+      Player.seekTo(seconds);
+    });
+
     vidChannel.on("new_annotation", (resp) => {
       this.renderAnnotation(msgContainer, resp);
     });
@@ -32,9 +46,7 @@ let Video = {
       .join()
       // .receive("ok", (resp) => console.log("joined the video channel", resp))
       .receive("ok", ({ annotations }) => {
-        annotations.forEach((annotation) =>
-          this.renderAnnotation(msgContainer, annotation),
-        );
+        this.scheduleMessages(msgContainer, annotations);
       })
       .receive("error", (reason) => console.log("join failed", reason));
   },
@@ -48,11 +60,37 @@ let Video = {
     let template = document.createElement("div");
     template.innerHTML = `
     <a href="#" data-seek="${this.esc(at)}">
+      [${this.formatTime(at)}]
       <b>${this.esc(user.username)}</b>: ${this.esc(body)}
     </a>
     `;
     msgContainer.appendChild(template);
     msgContainer.scrollTop = msgContainer.scrollHeight;
+  },
+
+  scheduleMessages(msgContainer, annotations) {
+    clearTimeout(this.schedulerTimer);
+    this.scheduleTimer = setTimeout(() => {
+      let currentTime = Player.getCurrentTime();
+      let remaining = this.renderAtTime(annotations, currentTime, msgContainer);
+      this.scheduleMessages(msgContainer, remaining);
+    }, 1000);
+  },
+
+  renderAtTime(annotations, seconds, msgContainer) {
+    return annotations.filter((annotation) => {
+      if (annotation.at > seconds) {
+        return true;
+      }
+      this.renderAnnotation(msgContainer, annotation);
+      return false;
+    });
+  },
+
+  formatTime(at) {
+    let date = new Date(null);
+    date.setSeconds(at / 1000);
+    return date.toISOString().substr(14, 5);
   },
 };
 export default Video;
