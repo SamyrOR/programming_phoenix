c9be8b2e9f6f405385cb7bc52384e2306e70ab2f
Samyr Ribeiro <saamyr@live.com>
2024-07-01 12:23:56 -0300

    chore(Dockerfile): created a dockerfile and a entrypoint.sh for the build

---
+++ rumbl_umbrella/entrypoint.sh
@@ -0,0 +1,8 @@
+# Compile assets
+MIX_ENV=prod mix assets.deploy
+
+# Custom tasks (like DB migrations)
+MIX_ENV=prod mix ecto.migrate
+
+# Finally run the server
+PORT=4001 MIX_ENV=prod mix phx.server
