455c6cb48c38170a1eed95842a3ec983f427a554
Samyr Ribeiro <saamyr@live.com>
2024-07-07 16:44:17 -0300

    chore(mix.exs): updated elixir version to 1.17.1

--- rumbl_umbrella/apps/info_sys/mix.exs
+++ rumbl_umbrella/apps/info_sys/mix.exs
@@ -9,7 +9,7 @@ defmodule InfoSys.MixProject do
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
       lockfile: "../../mix.lock",
-      elixir: "~> 1.16",
+      elixir: "~> 1.17.1",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
