2f0f47315a780da16953ed4fa0d58985207534b4
Samyr Ribeiro <saamyr@live.com>
2024-07-17 23:52:05 -0300

    chore(entrypoint): added mix task run to seed database

--- rumbl_umbrella/entrypoint.sh
+++ rumbl_umbrella/entrypoint.sh
@@ -4,6 +4,8 @@ MIX_ENV=prod mix assets.deploy
 
 # Custom tasks (like DB migrations)
 MIX_ENV=prod mix ecto.migrate
+MIX_ENV=prod mix run /apps/rumbl/priv/repo/seeds.exs
+MIX_ENV=prod mix run /apps/rumbl/priv/repo/backend_seeds.exs
 
 # Finally run the server
 PORT=4001 MIX_ENV=prod mix phx.server
